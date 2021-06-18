87c33a8226e8ea4e1dfa408869c542d8da2dc01e
Bill Hu <hubi@berkeley.edu>
2021-06-18 17:15:13 -0400

    redeploy

--- docs/main-es2015.js
+++ docs/main-es2015.js
@@ -1015,14 +1015,22 @@ let ProjectsComponent = class ProjectsComponent {
         this.projects = [
             {
                 id: 1,
-                title: 'Gitlet File Version Control',
-                desc: 'Version control system similar to git written in java',
-                githurl: 'https://github.com/hubi0111/cs61b/tree/master/proj2/gitlet',
-                imgUrl: 'assets/images/z1.png',
-                tech: 'Java'
+                title: 'Multi-Channel Authentication with DI',
+                desc: 'Multi-channel authentication for various authentication services',
+                githurl: 'https://github.com/hubi0111/Multi-channel-authentication-with-DJ-in-NODE-JS',
+                imgUrl: 'assets/images/z4.PNG',
+                tech: 'Node JS, React, MongoDB'
             },
             {
                 id: 2,
+                title: 'Dynamic Configurable Menu',
+                desc: 'Authenticate user and dynamically configure menu from user role',
+                githurl: 'https://github.com/hubi0111/Configurable-authorization-and-menu-navigation-in-NODE-JS',
+                imgUrl: 'assets/images/z5.png',
+                tech: 'Node JS, React, MongoDB'
+            },
+            {
+                id: 3,
                 title: 'Menu App',
                 desc: 'App to order food from restaurant with user roles',
                 githurl: 'https://github.com/hubi0111/Menu-App',
@@ -1030,20 +1038,20 @@ let ProjectsComponent = class ProjectsComponent {
                 tech: 'Angular, Java Spring Boot, MySQL'
             },
             {
-                id: 3,
+                id: 4,
+                title: 'Gitlet File Version Control',
+                desc: 'Version control system similar to git written in java',
+                githurl: 'https://github.com/hubi0111/cs61b/tree/master/proj2/',
+                imgUrl: 'assets/images/z1.png',
+                tech: 'Java'
+            },
+            {
+                id: 5,
                 title: 'Scheme',
                 desc: 'Custom scheme language interpreter built in python',
                 githurl: 'https://github.com/hubi0111/cs61a/tree/master/scheme',
                 imgUrl: 'assets/images/z27.png',
                 tech: 'Python'
-            },
-            {
-                id: 4,
-                title: 'Multi-Channel Authentication with DI',
-                desc: 'Multi-channel authentication for various authentication services',
-                githurl: 'https://github.com/hubi0111/Multi-channel-authentication-with-DJ-in-NODE-JS',
-                imgUrl: 'assets/images/z23.png',
-                tech: 'Node JS, React, MongoDB'
             },
         ];
     }
